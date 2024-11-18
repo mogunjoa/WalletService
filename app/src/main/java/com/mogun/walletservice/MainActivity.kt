34d0d6eff5babeaaedf96e3a369e6bfceb817e75
seungohjin <corrobolea@gmail.com>
2024-11-18 16:53:43 +0900

    update: 카드 상세 UI 구성 및 애니메이션 처리

--- app/src/main/java/com/mogun/walletservice/MainActivity.kt
+++ app/src/main/java/com/mogun/walletservice/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.mogun.walletservice
 
 import android.os.Bundle
+import android.view.View.OnClickListener
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.TransitionAdapter
 import com.mogun.walletservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +17,61 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.view = this
 
+        binding.thirdCard.setOnClickListener(getCardClickListener(R.id.thirdCardOnTop))
+        binding.secondCard.setOnClickListener(getCardClickListener(R.id.secondCardOnTop))
+        binding.firstCard.setOnClickListener(getCardClickListener(R.id.firstCardOnTop))
+    }
 
+    private fun getCardClickListener(@IdRes endStateId: Int) = OnClickListener {
+        with(binding) {
+            when(root.currentState) {
+                R.id.fanOut -> {
+                    root.setTransition(R.id.fanOut, R.id.firstCardOnTop)
+                    root.transitionToEnd()
+                    collapsedCardCompletedListener(endStateId)
+                }
+                R.id.thirdCardOnTop -> {
+                    if(R.id.thirdCardOnTop == endStateId) {
+
+                    } else {
+                        root.setTransition(R.id.thirdCardOnTop, endStateId)
+                        root.transitionToEnd()
+                    }
+                }
+                R.id.secondCardOnTop -> {
+                    if(R.id.secondCardOnTop == endStateId) {
+
+                    } else {
+                        root.setTransition(R.id.secondCardOnTop, endStateId)
+                        root.transitionToEnd()
+                    }
+                }
+                R.id.firstCardOnTop -> {
+                    if(R.id.firstCardOnTop == endStateId) {
+
+                    } else {
+                        root.setTransition(R.id.firstCardOnTop, endStateId)
+                        root.transitionToEnd()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun collapsedCardCompletedListener(@IdRes endStateId: Int) {
+        with(binding) {
+            root.setTransitionListener(object : TransitionAdapter() {
+                override fun onTransitionCompleted(
+                    motionLayout: MotionLayout?,
+                    currentId: Int
+                ) {
+                    if(currentId == R.id.firstCardOnTop) {
+                        root.setTransition(R.id.firstCardOnTop, endStateId)
+                        root.transitionToEnd()
+                    }
+                    root.setTransitionListener(null)
+                }
+            })
+        }
     }
 }
